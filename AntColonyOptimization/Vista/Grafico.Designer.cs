b275d1888f9419ff831640ed860661eb7ab16cb1
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-11-10 18:59:56 -0600

    creacion grafica de resultados clase

---
+++ AntColonyOptimization/Vista/Grafico.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AntColonyOptimization.Vista
+{
+    partial class Grafico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.grafica = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lb_titulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grafica)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grafica
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.grafica.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.grafica.Legends.Add(legend1);
+            this.grafica.Location = new System.Drawing.Point(33, 73);
+            this.grafica.Name = "grafica";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.grafica.Series.Add(series1);
+            this.grafica.Size = new System.Drawing.Size(585, 396);
+            this.grafica.TabIndex = 0;
+            this.grafica.Text = "chart1";
+            // 
+            // lb_titulo
+            // 
+            this.lb_titulo.AutoSize = true;
+            this.lb_titulo.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_titulo.Location = new System.Drawing.Point(230, 26);
+            this.lb_titulo.Name = "lb_titulo";
+            this.lb_titulo.Size = new System.Drawing.Size(165, 33);
+            this.lb_titulo.TabIndex = 1;
+            this.lb_titulo.Text = "Resultados";
+            // 
+            // Grafico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 509);
+            this.Controls.Add(this.lb_titulo);
+            this.Controls.Add(this.grafica);
+            this.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Grafico";
+            this.Text = "Grafico";
+            this.Load += new System.EventHandler(this.Grafico_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grafica)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafica;
+        private System.Windows.Forms.Label lb_titulo;
+    }
+}
---
+++ AntColonyOptimization/Vista/Grafico.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntColonyOptimization.Vista
+{
+    public partial class Grafico : Form
+    {
+        /**-------------------------------------------------------------------------------------------
+         * Atributos
+         *--------------------------------------------------------------------------------------------
+         **/
+
+        
+        /**-------------------------------------------------------------------------------------------
+         * Métodos
+         *--------------------------------------------------------------------------------------------
+         **/
+        private void Grafico_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+        public Grafico()
+        {
+            InitializeComponent();
+            grafica.Series.Clear();
+        }
+        public void crear_serie(String serie)
+        {
+            grafica.Series.Add(serie);
+            grafica.Series[serie].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+        }
+        public void crear_punto(int pos_serie, double x, double y)
+        {
+            grafica.Series[pos_serie].Points.AddXY(x, y);
+            
+        }
+        private void Chart_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
